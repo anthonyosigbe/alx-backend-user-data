bfbc0aa5b62f7813299c6a22725ac0574423da40
Anthony Osigbe <thronesofglory@gmail.com>
2024-05-28 19:54:02 +0000

    15. User profile tasks completed

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -36,6 +36,7 @@ def users() -> str:
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
 
+
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
     """POST /sessions
@@ -50,6 +51,7 @@ def login() -> str:
     response.set_cookie("session_id", session_id)
     return response
 
+
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> str:
     """DELETE request to /sessions endpoint.
@@ -62,3 +64,16 @@ def logout() -> str:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect("/")
+
+
+@app.route("/profile", methods=["GET"], strict_slashes=False)
+def profile() -> str:
+    """GET request to /profile
+    Return:
+        - The user's profile details.
+    """
+    session_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(session_id)
+    if user is None:
+        abort(403)
+    return jsonify({"email": user.email})
